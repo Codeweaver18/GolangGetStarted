09ea8a5cc5541a2e6c9ecd45e4cbd09317569d4c
Codeweaver <jereco11@gmail.com>
2019-05-19 04:10:55 +0100

    Create Helloworld.go
    changes

---
+++ Day 1/Helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	
+)
+
+func main()  {
+	fmt.Println("Hello world, welcome to go programing")
+	fmt.Println(strings.ToUpper("Hellowolrd from second line"))
+}
